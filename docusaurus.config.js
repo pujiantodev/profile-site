5e854656ba75ac30d332e9e896a0e3dfaa523ce2
Eko <ekoxyz15@gmail.com>
2023-11-02 00:28:14 +0800

    update : config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://pujiantodev.github.io/profile-site",
+  url: "https://pujiantodev.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/profile-site/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
